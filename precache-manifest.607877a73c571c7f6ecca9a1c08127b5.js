b9f19908f5136a0cca198672ea0fe72c74c2ca73
OMKAR <omkargupta702@gmail.com>
2024-11-17 01:02:57 +0530

    first commit

---
+++ precache-manifest.607877a73c571c7f6ecca9a1c08127b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b680f7e23911c24be5821469355c7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07fd379f4c643ae21d4",
+    "url": "/static/css/main.51b506c9.chunk.css"
+  },
+  {
+    "revision": "8db21a14fcc2dbc3bc98",
+    "url": "/static/js/2.887c9e4d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.887c9e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07fd379f4c643ae21d4",
+    "url": "/static/js/main.deff4c54.chunk.js"
+  },
+  {
+    "revision": "e05f2692aa143af5cea3",
+    "url": "/static/js/runtime-main.38f2a1df.js"
+  }
+]);
